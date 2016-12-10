aa3ae3a7f6bba2e4adebf7256732b9887b0bd120
ENDEAVOUR\TowneJ <towja@icloud.com>
2016-12-10 23:25:05 +1030

    Created a very boring unstyled, however semi-functional menu. Scripts are written, values must still be set.

---
+++ Assets/Scripts - Jarod/OpenScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OpenScene : MonoBehaviour {
+
+    public Scene scene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Open()
+    {
+        if (scene != null)
+        {
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
