befc57deb5e3f4983bc118775bd790556d6e22d2
Lachie Simpson <lachiesimpson1@gmail.com>
2016-12-11 11:46:31 +1030

    player controler and other shit

---
+++ lachieScene/Assets/murderSystem.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class murderSystem : MonoBehaviour {
+
+	public Light light;
+	public GameObject murderer;
+	public List<GameObject> guests = new List<GameObject> ();
+	public List<GameObject> murderedGuests = new List<GameObject>();
+	public float timeInterval = 30;
+
+	// Use this for initialization
+	void Start () {
+		int x = Random.Range (0, guests.Count);
+		murderer = guests [x];
+		guests.RemoveAt (x);
+		murderedGuests.RemoveAt (x);
+		print (murderer.name + ": Is the murderer");
+		InvokeRepeating ("KillGuest", 0f, timeInterval);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (guests.Count <= 6 || murderer == null) {
+			CancelInvoke();
+		}
+
+		if (guests.Count <= 6 && murderer != null) {
+			print ("you lose");
+		}
+
+		if (murderer == null) {
+			print ("you win");
+		}
+	}
+
+	void KillGuest(){
+		light.enabled = false;
+		int i = Random.Range (0, guests.Count);
+		Vector2 tempGuestPos = guests[i].transform.position;
+		Destroy (guests [i]);
+		//Instantiate (murderedGuests[i], tempGuestPos, Quaternion.identity);
+		guests.RemoveAt (i);
+		timeInterval -= 5;
+		StartCoroutine ("lightFlash");
+
+	}
+
+	IEnumerator lightFlash(){
+		yield return new WaitForSeconds (0.5f);
+		light.enabled = true;
+	}
+}
