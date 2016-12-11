c7883c658a5eaa630d9d0d2ef5fb23b50473d494
ENDEAVOUR\TowneJ <towja@icloud.com>
2016-12-11 14:40:38 +1030

    Finalised clues, they will need testing but I do not have the ability to test it right now. You guys need to test it.

--- Assets/Scripts/Clues.cs
+++ Assets/Scripts/Clues.cs
@@ -109,22 +109,24 @@ public class Clues : MonoBehaviour {
 		}
 		if (!guestAlreadyDead) {
 			List<string> deathMethods = new List<string> ();
-			foreach (string s in careerKills[guest.profession]) {
+			foreach (string s in careerKills[guest.profession.ToString()]) {
 				deathMethods.Add (s);
 			}
-			foreach (string s in hobbyKills[guest.hobby]) {
+			foreach (string s in hobbyKills[guest.hobby.ToString()]) {
 				deathMethods.Add (s);
 			}
 
 			int deathNum = Random.Range (0, (deathMethods.Count - 1));
-			Clue deathClue = new Clue (guest, deathMethods[deathNum], 0);
+			Clue deathClue = new Clue (guest, deathMethods [deathNum], false);
 			returnList.Add (deathClue);
 
 			int appearanceClue = Random.Range (0, 3);
 			if (appearanceClue == 3) {
-				//TODO: Write appearance clue code.
+				//deathClue.setAppearanceText(***MURDERER***);
 			}
 			return returnList;
+		} else {
+			return null;
 		}
 	}
 }
@@ -135,17 +137,80 @@ public class Clue {
 	bool isAppearance;
 	GuestClass guest;
 	string deathType;
-	string clueText;
+	string deathText;
+
+	string appearanceText;
+
 
 	public Clue(GuestClass _guest, string _deathType, bool _isAppearance) {
 		isAppearance = _isAppearance;
 		guest = _guest;
 		deathType = _deathType;
-
 		//TODO Define clue text
+		string deathText = "";
+
+		switch (deathType) {
+		case "Cut":
+			deathText = "" + guest.name + " appears to have died from multiple cut wounds.";
+			break;
+		case "Poison":
+			deathText = "From what you can tell, " + guest.name + " was poisoned.";
+			break;
+		case "Stab":
+			deathText = "There are multiple stab wounds in " + guest.name + "'s body. This seems to be the cause of death.";
+			break;
+		case "Strangle":
+			deathText = "" + guest.name + " has bruising around their neck; it appears they were strangled to death.";
+			break;
+		case "Bludge":
+			deathText = "It seems as if " + guest.name + " was bludgeoned to death!";
+			break;
+		case "Shoot":
+			deathText = "" + guest.name + " has a bullet wound, they must have been shot.";
+			break;
+		default:
+			deathText = "" + guest.name + "'s death appears to be a mystery!";
+			break;
+		}
 	}
 
-	public string getClue() {
-		return clueText;
+	public string getDeathClue() {
+		return deathText;
 	}
+
+	public void setAppearanceText(GuestClass murderer) {
+		int descriptorRand = Random.Range(0, 1);
+		string appearanceClueText = "";
+		if (descriptorRand == 1) {
+			switch (murderer.Accessory.ToString ()) {
+			case "Hat":
+				appearanceClueText = "I think the murderer was wearing a hat.";
+				break;
+			case "Glasses":
+				appearanceClueText = "I believe I saw the murderer wearing glasses!";
+				break;
+			case "WorkAccessory":
+				appearanceClueText = "They had some sort of technical instrument.";
+				break;
+			default:
+				appearanceClueText = "The murderer didn't seem to have any sort of accessory.";
+				break;
+			}
+		} else {
+			switch(murderer.Blemish.ToString()) {
+			case "Tattoo": 
+				appearanceClueText = "I saw that the murderer had a tattoo.";
+				break;
+			case "Scar":
+				appearanceClueText = "They had some sort of horrible scar!";
+				break;
+			case "SkinCondition":
+				appearanceClueText = "The murderer had the most horrible skin.";
+				break;
+			default:
+				appearanceClueText = "The murderer had plain skin.";
+				break;
+		}
+	}
+}
 }
