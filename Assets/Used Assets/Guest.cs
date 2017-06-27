cd472aaa6d3f874b553ab862204cf25b6c38ebf4
Jarod Towner <Jarod.Towner@endeavour.sa.edu.au>
2017-06-27 18:15:31 +0930

    Moved all used asssets into a "Used Assets" Folder

---
+++ Assets/Used Assets/Guest.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Guest : MonoBehaviour {
+
+	// Use this for initialization
+	public GuestClass guestClass;
+	public GuestClass[] GuestArray;
+	public bool dead;
+	public Transform Panel;
+	public Transform Portrait;
+	public murderSystem murderScript;
+
+	playerController playerScript;
+	Transform deathPanel;
+
+	void Start () {
+		playerScript = GameObject.FindWithTag ("Player").GetComponent<playerController> ();
+		deathPanel = transform.GetChild (0).transform.GetChild (1);
+		Portrait = transform.Find ("Canvas").Find ("Portrait");
+		murderScript = GameObject.Find ("Gm").GetComponent<murderSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DisplayText ();
+		if (dead) {
+			this.gameObject.layer = LayerMask.NameToLayer("deadGuest");
+			Physics2D.IgnoreLayerCollision(8,9,true);
+		}
+	}
+
+	public void setText () {
+		Panel = transform.GetChild (0).transform.GetChild (0);
+		Panel.transform.GetChild (0).GetComponent<Text> ().text = guestClass.name;
+
+		/*string tmp3 = guestClass.profession.ToString();
+		string[] splitString3 = tmp3.Split ('_');
+		string z = "";
+		for (int i = 0; i < splitString3.Length; i++) {
+			z += splitString3 [i] + " ";
+		}*/
+
+		Panel.transform.GetChild (1).GetComponent<Text> ().text = guestClass.profession.ToString ();
+
+		if (guestClass.Accessory.ToString () == "no_accesories") {
+			Panel.transform.GetChild (2).GetComponent<Text> ().text = "Is wearing no accessories";
+		}else{
+			/*string tmp = guestClass.Accessory.ToString ();
+			string tmp2 = guestClass.Blemish.ToString ();
+			string y = "";
+			string x = "";
+			string[] splitString2 = tmp2.Split ('_');
+			string[] splitString = tmp.Split ('_');
+
+			for (int i = 0; i < splitString.Length; i++) {
+				x += " "+splitString [i];
+			}
+
+			for (int i = 0; i < splitString2.Length; i++) {
+				y += " "+splitString2 [i];
+			}*/
+
+			Panel.transform.GetChild (2).GetComponent<Text> ().text = "Is wearing a" + guestClass.Accessory.ToString ();;
+			Panel.transform.GetChild (3).GetComponent<Text> ().text = "Has a" + guestClass.Blemish.ToString ();
+		}
+
+		if (guestClass.Accessory.ToString () == "glasses") {
+			Panel.transform.GetChild (2).GetComponent<Text> ().text = "Is wearing glasses";
+		}
+	}
+
+	public void setDeathText (string deathText) {
+		deathPanel.GetChild (0).GetComponent<Text> ().text = deathText;
+	}
+
+	public void setHintText (string hintText) {
+		print (guestClass.name);
+		Panel.transform.GetChild (4).GetComponent<Text> ().text = hintText;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player" && playerScript.nearbyGuests.Contains(transform) == false) {
+			playerScript.addGuest (transform);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player" && Panel != null && deathPanel != null) {
+			playerScript.removeGuest (transform);
+		}
+	}
+
+	void DisplayText() {
+		if (playerScript.nearestGuest == gameObject) {
+			if (dead == false && Panel != null) {
+				Panel.gameObject.SetActive (true);
+			} else if (dead == true && deathPanel != null) {
+				print (dead.ToString ());
+				deathPanel.gameObject.SetActive (true);
+			}
+
+			if (Portrait != null) {
+				Portrait.gameObject.SetActive (true);
+			}
+		} else {
+			if (Panel != null && deathPanel != null) {
+				Panel.gameObject.SetActive (false);
+				deathPanel.gameObject.SetActive (false);
+			}
+			if (Portrait != null) {
+				Portrait.gameObject.SetActive (false);
+			}
+		}
+
+	}
+	public void Die() {
+		dead = true;
+		transform.Find ("Graphics").GetComponent<Animator> ().enabled = false;
+		transform.Find ("Graphics").GetComponent<SpriteRenderer> ().sprite = guestClass.deadSprite;
+	}
+}
---
+++ Assets/Used Assets/GuestScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class GuestClass {
+	public string name;
+	public enum professions {Doctor, Scientist, Author, Home_maker, Chef, Politician, Film_maker, Veteran};
+	public professions profession;
+	public enum Accessories {hat, glasses, work_accessory, no_accesories};
+	public Accessories Accessory;
+	public enum Blemishes {Tattoo, Scar, skin_condition, clean_skin};
+	public Blemishes Blemish;
+
+
+	public GameObject GuestGameObject;
+	public Sprite idleSprite;
+	public Sprite Portrait;
+	public Sprite deadSprite;
+	public RuntimeAnimatorController animationController;
+}
+
+public class GuestScript : MonoBehaviour {
+	public GuestClass[] GuestArray;
+	public Transform[] SpawnPoints;
+	public GameObject GuestPrefab;
+	public murderSystem murderScript;
+	public Clues clueScript;
+	public float maxClueDistance;
+	// Use this for initialization
+	void Start () {
+		murderScript = GetComponent<murderSystem> ();
+		SpawnGuests ();
+		murderScript.enabled = true;
+		clueScript = GetComponent<Clues> ();
+	}
+
+	// Update is called once per frame
+	void SpawnGuests() {
+		int i = 0;
+		murderScript.guests = new List<GameObject> ();
+		foreach (var item in GuestArray) {
+			if (i <= SpawnPoints.Length) { 
+				GameObject SpawnedGuest = Instantiate (GuestPrefab, SpawnPoints[i].position,GuestPrefab.transform.rotation) as GameObject;
+				SpawnedGuest.transform.Find ("Graphics").rotation = SpawnPoints [i].rotation;
+				SpawnedGuest.GetComponent<Guest> ().guestClass = item;
+				SpawnedGuest.GetComponent<Guest> ().setText ();
+				SpawnedGuest.transform.Find ("Graphics").GetComponent<Animator> ().runtimeAnimatorController = item.animationController;
+				SpawnedGuest.transform.Find ("Graphics").GetComponent<SpriteRenderer> ().sprite = item.idleSprite;
+				SpawnedGuest.transform.Find ("Canvas").Find ("Portrait").GetComponent<SpriteRenderer> ().sprite = item.Portrait;
+				item.GuestGameObject = SpawnedGuest;
+				murderScript.guests.Add (SpawnedGuest);
+			}
+			i++;
+		}
+		gameObject.GetComponent<AnimationManager> ().guests = murderScript.guests;
+	}
+
+	public void dropHint(GameObject deadGuest, string clue) {
+		float lowestDist = 100;
+		GameObject closestGuest = deadGuest;
+		foreach (var item in GuestArray) {
+			
+			if(Vector2.Distance(item.GuestGameObject.transform.position, deadGuest.transform.position) < lowestDist && item.GuestGameObject.GetComponent<Guest>().dead ==false) {
+				closestGuest = item.GuestGameObject;
+				lowestDist = Vector2.Distance (item.GuestGameObject.transform.position, deadGuest.transform.position);
+			}
+		}
+		closestGuest.GetComponent<Guest> ().setHintText (clue);
+	}
+}
