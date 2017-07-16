0902205ac404395a5bc183052b7b625f9fd585ba
Jarod Towner <towja@icloud.com>
2017-07-16 13:17:48 +0930

    Created and animated the three floors of the hotel level and created a staircase prefab.

---
+++ Assets/Scripts/Staircase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staircase : MonoBehaviour {
+	public GameObject Destination;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Player")) {
+			GameObject player = other.gameObject;
+			player.transform.position = Destination.transform.position;
+		}
+	}
+}
